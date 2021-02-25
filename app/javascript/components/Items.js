08c0d778aec96cd450489274e3b35aadb6c32ca4
Trevor von Hake <tvonhake@outlook.com>
2021-02-24 18:15:41 -0700

    still working on items.js

--- app/javascript/components/Items.js
+++ app/javascript/components/Items.js
@@ -1,32 +1,17 @@
 import React from 'react'
 
-const Items = (props) => {
-  const renderItems = () =>{
-      return props.items.map( item => {
-          return (
-            <div className="item-container">
-              <div id="item-content">
-                <h1>Quantity: {props.item.quantity}</h1>
-                <p>Comment: {props.item.comment}</p>
-                <hr class="itemSeparator"></hr>
-              </div>
-                  
-                {/* <a href={`/items/${note.id}`}>Open</a>
-                <a href={`/items/${note.id}/edit`}>Edit</a>
-                <a href={`/items/${note.id}`} data-method='delete'>Delete note</a> */}
-            </div>
-          )
-      })
-  }
-
-  return(
-      <div id='main'>
-          <h1>Items</h1>
-          <div id='nav'>
-              {/* <a href={`/items/new`}>New Note Form</a> */}
-          </div>
-          {renderItems()}
-      </div>   
+const Items = () => {
+  
+  return (
+    <div id='main'>
+      <h1>Items</h1>
+        <div id='nav'>
+          <a href='/departments'>Return to Departments</a>
+        </div>
+        
+        {/* <a href={`/items/${props.item.id}/edit`}>Edit</a>
+        <a href={`/items/${props.item.id}`} data-method='delete'>Delete item</a> */}
+    </div>
   )
 }
 
